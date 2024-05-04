6c4392fb72322aa2966c9c1ae54d29c26e8db5ac
Oekn <96504783+Kenta-Oe@users.noreply.github.com>
2024-05-05 00:31:59 +0900

    Add files via upload

--- rakuten_login/rakuten_auto_login.py
+++ rakuten_login/rakuten_auto_login.py
@@ -1,7 +1,3 @@
-#
-# 楽天証券サイトへログインしてデータを取得する
-#
-
 import json
 from selenium.webdriver import Safari
 from selenium.webdriver.common.by import By
@@ -12,17 +8,17 @@ from bs4 import BeautifulSoup
 
 
 # 設定ファイルからログイン情報を取得
-login_info = json.load(open("login_info.json", "r", encoding="utf-8"))
+login_info = json.load(open("fail_path", "r", encoding="utf-8"))
 
 # ログインサイト名
-site_name = "sec_rakuten"
+site_name = "URL"
 
 # ログイン画面URL
-url_login = login_info[site_name]["url"]
+url_login = login_info["bank_syouken"]["url"]
 
 # ユーザー名とパスワードの指定
-USER = login_info[site_name]["id"]
-PASS = login_info[site_name]["pass"]
+USER = login_info["bank_syouken"]["id"]
+PASS = login_info["bank_syouken"]["pass"]
 
 # Safariを起動する
 browser = Safari()
@@ -43,20 +39,40 @@ frm = browser.find_element(by=By.NAME, value="loginform")
 frm.submit()
 
 # ページロード完了まで待機
-#WebDriverWait(browser, 20).until(
+WebDriverWait(browser, 20).until(
     # 保有商品の金額を取得するための待機
-#    ec.presence_of_element_located((By.CSS_SELECTOR, '//*[@id="balance_data_actual_data"]/section/table[1]/tbody/tr[2]/td[1]/p/span[1]}')
-# ))
-
+    ec.presence_of_element_located((By.XPATH, 'browser_XPATH'))
+)
 
 
 # 保有商品の金額の取得
-#set_value = browser.find_element(By.CSS_SELECTOR, '//*[@id="balance_data_actual_data"]/section/table[1]/tbody/tr[2]/td[1]/p/span[1]}').text
+max_attempts = 20
+attempts = 0
 
-#print(f'保有商品の金額: {asset_value}')
+while attempts < max_attempts:
+    try:
+        # 要素がロードされるのを待つ
+        set_value01 = browser.find_element(By.XPATH, 'browser_XPATH').text
+        set_value02 = browser.find_element(By.XPATH, 'browser_XPATH').text
+        set_value03 = browser.find_element(By.XPATH, 'browser_XPATH').text
+        set_value04 = browser.find_element(By.XPATH, 'browser_XPATH').text
+
+        # 要素のテキストが '-' でないことを確認
+        if set_value01 != '-' and set_value02 != '-' and set_value03 != '-' and set_value04 != '-':
+            print(f'日本円預かり金: {set_value01}')
+            print(f'投資信託: {set_value02}')
+            print(f'外貨預り金合計: {set_value03}')
+            print(f'米国株式: {set_value04}')
+            break
+        else:
+            print("値が '-' なので再試行します。")
+    except TimeoutException:
+        print("指定した要素が見つかりません。再試行します。")
+    
+    attempts += 1
 
 
-response = requests.get('https://www.rakuten-sec.co.jp/session_error.html')
+response = requests.get('URL')
 soup = BeautifulSoup(response.content, 'html.parser')
 
 span_element = soup.find('span', class_='pcmm-m1-home-assets-table__amount')
